cec0f0c56ae3afdb844cbb30d828be01e5da4599
Guus Waals <_@guusw.nl>
2022-07-03 20:37:15 +0200

    Add wireframe overlay rendering
    # Conflicts:
    #	src/gfx/shader/generator.cpp

---
+++ src/gfx/wireframe.hpp
@@ -0,0 +1,31 @@
+#ifndef GFX_WIRE_MESH
+#define GFX_WIRE_MESH
+
+#include "fwd.hpp"
+#include <map>
+
+namespace gfx {
+struct WireframeMeshGenerator {
+  MeshPtr mesh;
+
+  WireframeMeshGenerator() {}
+  MeshPtr generate();
+};
+
+struct WireframeRenderer {
+private:
+  FeaturePtr wireframeFeature;
+
+  struct MeshCacheEntry {
+    MeshPtr wireMesh;
+    std::weak_ptr<Mesh> weakMesh;
+  };
+  std::map<Mesh *, MeshCacheEntry> meshCache;
+
+public:
+  WireframeRenderer(bool showBackfaces = false);
+  void overlayWireframe(DrawQueue &queue, DrawablePtr drawable);
+};
+} // namespace gfx
+
+#endif // GFX_WIRE_MESH
