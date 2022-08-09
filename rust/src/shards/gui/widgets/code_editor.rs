467a414f36d3b19d297377d2d852ff1436fb27e0
Nicolas Musset <musset.nicolas@gmail.com>
2022-08-09 13:25:23 +0200

    first attempt (mostly copying code from egui sample)

--- rust/src/shards/gui/widgets/code_editor.rs
+++ rust/src/shards/gui/widgets/code_editor.rs
@@ -23,8 +23,13 @@ use crate::types::Var;
 use crate::types::BOOL_TYPES_SLICE;
 use crate::types::NONE_TYPES;
 use crate::types::STRING_TYPES;
+use egui::epaint::text::layout;
+use egui::text::LayoutJob;
 use std::cmp::Ordering;
 use std::ffi::CStr;
+use syntect::highlighting::Theme;
+use syntect::highlighting::ThemeSet;
+use syntect::parsing::SyntaxSet;
 
 lazy_static! {
   static ref CODEEDITOR_PARAMETERS: Parameters = vec![(
@@ -192,6 +197,16 @@ impl Shard for CodeEditor {
 
   fn activate(&mut self, _context: &Context, _input: &Var) -> Result<Var, &str> {
     if let Some(ui) = util::get_current_parent(*self.parents.get())? {
+      let theme = if ui.style().visuals.dark_mode {
+        CodeTheme::dark()
+      } else {
+        CodeTheme::light()
+      };
+      let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
+        let mut layout_job = highlight(ui.ctx(), &theme, string, "Clojure");
+        layout_job.wrap.max_width = wrap_width;
+        ui.fonts().layout_job(layout_job)
+      };
       let text = &mut if self.mutable_text {
         VarTextBuffer::Editable(self.variable.get_mut())
       } else {
@@ -200,8 +215,11 @@ impl Shard for CodeEditor {
       let code_editor = egui::TextEdit::multiline(text)
         .code_editor()
         .desired_rows(10)
-        .desired_width(f32::INFINITY);
-      let response = ui.add(code_editor);
+        .desired_width(f32::INFINITY)
+        .layouter(&mut layouter);
+      let response = egui::ScrollArea::vertical()
+        .show(ui, |ui| ui.add(code_editor))
+        .inner;
 
       if response.changed() || response.lost_focus() {
         Ok(*self.variable.get())
@@ -212,4 +230,167 @@ impl Shard for CodeEditor {
       Err("No UI parent")
     }
   }
+}
+
+/// Memoized Code highlighting
+fn highlight(ctx: &egui::Context, theme: &CodeTheme, code: &str, language: &str) -> LayoutJob {
+  impl egui::util::cache::ComputerMut<(&CodeTheme, &str, &str), LayoutJob> for Highlighter {
+    fn compute(&mut self, (theme, code, language): (&CodeTheme, &str, &str)) -> LayoutJob {
+      self.highlight(theme, code, language)
+    }
+  }
+
+  type HighlightCache<'a> = egui::util::cache::FrameCache<LayoutJob, Highlighter>;
+
+  let mut memory = ctx.memory();
+  let highlight_cache = memory.caches.cache::<HighlightCache<'_>>();
+  highlight_cache.get((theme, code, language))
+}
+
+#[derive(Hash)]
+struct CodeTheme {
+  dark_mode: bool,
+  syntect_theme: SyntectTheme,
+}
+
+impl Default for CodeTheme {
+  fn default() -> Self {
+    Self::dark()
+  }
+}
+
+impl CodeTheme {
+  pub fn dark() -> Self {
+    Self {
+        dark_mode: true,
+        syntect_theme: SyntectTheme::Base16MochaDark,
+    }
+  }
+
+  pub fn light() -> Self {
+    Self {
+      dark_mode: false,
+      syntect_theme: SyntectTheme::SolarizedLight,
+    }
+  }
+}
+
+struct Highlighter {
+  syntaxes: SyntaxSet,
+  themes: ThemeSet,
+}
+
+impl Default for Highlighter {
+  fn default() -> Self {
+    // FIXME: build from definition of our language
+    let ss = SyntaxSet::load_defaults_newlines();
+    for s in ss.syntaxes() {      
+      shlog!("Lang: {}, Extensions: {}", s.name, s.file_extensions.join(","));
+    }
+    Self {
+      syntaxes: SyntaxSet::load_defaults_newlines(),
+      themes: ThemeSet::load_defaults(),
+    }
+  }
+}
+
+impl Highlighter {
+  fn highlight(&self, theme: &CodeTheme, text: &str, language: &str) -> LayoutJob {
+    self
+      .highlight_impl(theme, text, language)
+      .unwrap_or_else(|| {
+        LayoutJob::simple(
+          text.into(),
+          egui::FontId::monospace(14.0),
+          if theme.dark_mode {
+            egui::Color32::LIGHT_GRAY
+          } else {
+            egui::Color32::DARK_GRAY
+          },
+          f32::INFINITY,
+        )
+      })
+  }
+
+  fn highlight_impl(&self, theme: &CodeTheme, text: &str, language: &str) -> Option<LayoutJob> {
+    use syntect::easy::HighlightLines;
+    use syntect::highlighting::FontStyle;
+    use syntect::util::LinesWithEndings;
+
+    let syntax = self
+      .syntaxes
+      .find_syntax_by_name(language)
+      .or_else(|| self.syntaxes.find_syntax_by_extension(language))?;
+
+    let theme = theme.syntect_theme.syntect_key_name();
+    let mut h = HighlightLines::new(syntax, &self.themes.themes[theme]);
+
+    use egui::text::{LayoutSection, TextFormat};
+
+    let mut job = LayoutJob {
+      text: text.into(),
+      ..Default::default()
+    };
+
+    for line in LinesWithEndings::from(text) {
+      for (style, range) in h.highlight_line(line, &self.syntaxes).ok()? {
+        let fg = style.foreground;
+        let text_color = egui::Color32::from_rgb(fg.r, fg.g, fg.b);
+        let italics = style.font_style.contains(FontStyle::ITALIC);
+        let underline = style.font_style.contains(FontStyle::ITALIC);
+        let underline = if underline {
+          egui::Stroke::new(1.0, text_color)
+        } else {
+          egui::Stroke::none()
+        };
+        job.sections.push(LayoutSection {
+          leading_space: 0.0,
+          byte_range: as_byte_range(text, range),
+          format: TextFormat {
+            font_id: egui::FontId::monospace(14.0),
+            color: text_color,
+            italics,
+            underline,
+            ..Default::default()
+          },
+        });
+      }
+    }
+
+    Some(job)
+  }
+}
+
+#[derive(Hash)]
+enum SyntectTheme {
+  Base16EightiesDark,
+  Base16MochaDark,
+  Base16OceanDark,
+  Base16OceanLight,
+  InspiredGitHub,
+  SolarizedDark,
+  SolarizedLight,
+}
+
+impl SyntectTheme {
+  fn syntect_key_name(&self) -> &'static str {
+    match self {
+      Self::Base16EightiesDark => "base16-eighties.dark",
+      Self::Base16MochaDark => "base16-mocha.dark",
+      Self::Base16OceanDark => "base16-ocean.dark",
+      Self::Base16OceanLight => "base16-ocean.light",
+      Self::InspiredGitHub => "InspiredGitHub",
+      Self::SolarizedDark => "Solarized (dark)",
+      Self::SolarizedLight => "Solarized (light)",
+    }
+  }
+}
+
+fn as_byte_range(whole: &str, range: &str) -> std::ops::Range<usize> {
+  let whole_start = whole.as_ptr() as usize;
+  let range_start = range.as_ptr() as usize;
+  assert!(whole_start <= range_start);
+  assert!(range_start + range.len() <= whole_start + whole.len());
+  let offset = range_start - whole_start;
+  offset..(offset + range.len())
 }
